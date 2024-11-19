ad398a597b055c73e3343580868c0e793c192f01
obedjohannes <obedjohannes@OBEDJOHANNEF542>
2024-11-19 13:48:56 -0500

    Add project files.

---
+++ testEnviroment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testEnviroment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
